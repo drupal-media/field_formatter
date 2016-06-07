b360aa0491faef2acbfb39afbce0f1b8fb4ce0c2
Daniel Wehner <dawehner@googlemail.com>
2016-06-07 08:26:31 +0200

    add the field link formatter

---
+++ src/Plugin/Field/FieldFormatter/FieldLink.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\field_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Render\Element;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides the field link formatter.
+ *
+ * @FieldFormatter(
+ *   id = "field_link",
+ *   module = "field_linker",
+ *   label = @Translation("Field linker"),
+ *   field_types = {
+ *   },
+ * )
+ *
+ * 
+ */
+class FieldLink extends FieldWrapperBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $field_output = $this->getFieldOutput($items, $langcode);
+
+    $elements = [];
+    foreach (Element::children($field_output) as $key) {
+      $elements[$key] = [
+        '#type' => 'link',
+        '#url' => $items->getEntity()->toUrl(),
+        '#title' => $field_output[$key],
+      ];
+    }
+    return $elements;
+  }
+
+}
